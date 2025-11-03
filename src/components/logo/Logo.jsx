99a4016cba3077594497af6ae273f109414bf5cb
Faisal Zaid <zaidalideveloper@gmail.com>
2025-11-03 15:00:52 +0300

    final

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -7,7 +7,7 @@ export default function Logo() {
   return (
     <div className="img-pic">
 <img src={homePhoto} alt='TruHome Apparels' />
-<div className="text">TruHome Apparels</div>
+<div className="text"></div>
     </div>
   )
 }
