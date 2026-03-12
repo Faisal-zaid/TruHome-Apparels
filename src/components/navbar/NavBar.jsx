5aea819b98361f0cc13f39556bee6bde8c043609
Faisal Zaid <zaidalideveloper@gmail.com>
2026-03-12 11:38:52 +0300

    updated admin section

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,26 +1,26 @@
 import React, { useState } from "react";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
-import AdminPanel from "../../pages/AdminPanel"; // import admin panel
+import AdminLogin from "../../pages/AdminLogin";
 
-export default function NavBar() {
+export default function NavBar({ onAdminLogin }) {
+
   const [isOpen, setIsOpen] = useState(false);
-  const [showAdmin, setShowAdmin] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
 
   function handleAdminClick() {
-    const password = prompt("Enter admin password:");
-    if (password === "mamaleroy") {
-      setShowAdmin(true);
-    } else {
-      alert("oops you aint the admin");
-    }
+    setShowLogin(true);
   }
 
   return (
     <div className="main">
+
       <div className="shop-name">
+
         <div className="jina">TRUHOME APPARELS</div>
+
         <div className="icons">
+
           {/* Shopping cart icon */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -33,6 +33,7 @@ export default function NavBar() {
               d="M7 22q-.825 0-1.412-.587T5 20t.588-1.412T7 18t1.413.588T9 20t-.587 1.413T7 22m10 0q-.825 0-1.412-.587T15 20t.588-1.412T17 18t1.413.588T19 20t-.587 1.413T17 22M6.15 6l2.4 5h7l2.75-5z"
             />
           </svg>
+
 
           {/* Admin icon */}
           <svg
@@ -52,25 +53,52 @@ export default function NavBar() {
             </g>
           </svg>
 
-          <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+
+          {/* Hamburger */}
+          <div
+            className="hamburger"
+            onClick={() => setIsOpen(!isOpen)}
+          >
             <div className={`bar ${isOpen ? "open" : ""}`}></div>
             <div className={`bar ${isOpen ? "open" : ""}`}></div>
             <div className={`bar ${isOpen ? "open" : ""}`}></div>
           </div>
+
         </div>
+
       </div>
 
+
+      {/* Navigation links */}
       <nav className={`navs ${isOpen ? "active" : ""}`}>
         <ul>
+
           <li><Link to="/">HOME</Link></li>
+
           <li><Link to="/pajamas">PAJAMAS</Link></li>
+
           <li><Link to="/nightdress">NIGHT DRESS</Link></li>
+
           <li><Link to="/rompers">ROMPERS</Link></li>
+
           <li><Link to="/bathrobes">BATHROBES</Link></li>
+
         </ul>
       </nav>
 
-      {showAdmin && <AdminPanel />}
+
+      {/* Admin Login Modal */}
+      {showLogin && (
+        <AdminLogin
+          onLogin={(success) => {
+            if (success) {
+              setShowLogin(false);
+              onAdminLogin(true);
+            }
+          }}
+        />
+      )}
+
     </div>
   );
 }
