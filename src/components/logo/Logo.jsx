d698918406ab1ae4e092dd3fad9600850386f6d8
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-11 10:53:01 +0300

    removed image hero section

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-import homePhoto from "../../photo/home.jpeg"
+
 import "./Logo.css"
 
 
 export default function Logo() {
   return (
-//     <div className="img-pic">
-// {/* <img src={homePhoto} alt='TruHome Apparels' />
-// <div className="text"></div> */}
 
 
-//     </div>
+
 <div className='hero-section'>
 
 </div>
