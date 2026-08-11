a6e762284a8c256461d7553514fb62d7f3a61543
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-11 11:12:12 +0300

    commented out all code from logo

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+// import React from "react";
 
-import "./Logo.css";
+// import "./Logo.css";
 
-export default function Logo() {
-  return <div className="hero-section">
-    <div className="text"> TruHome Apparels </div>
-  </div>;
-}
+// export default function Logo() {
+//   return <div className="hero-section">
+//     <div className="text"> TruHome Apparels </div>
+//   </div>;
+// }
