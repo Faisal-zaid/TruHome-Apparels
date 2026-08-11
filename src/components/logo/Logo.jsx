481cba5f1c3c0ee225ef0f51ccb90916c9eda412
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-11 10:53:45 +0300

    added header

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,15 +1,9 @@
-import React from 'react'
+import React from "react";
 
-import "./Logo.css"
-
+import "./Logo.css";
 
 export default function Logo() {
-  return (
-
-
-
-<div className='hero-section'>
-
-</div>
-  )
+  return <div className="hero-section">
+    <div className="text"> TruHome Apparels </div>
+  </div>;
 }
