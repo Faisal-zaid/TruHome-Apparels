5640b2a689cb47d617c46e4bee3f962a3ff3937e
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-11 10:48:22 +0300

    added new div

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -5,11 +5,14 @@ import "./Logo.css"
 
 export default function Logo() {
   return (
-    <div className="img-pic">
-{/* <img src={homePhoto} alt='TruHome Apparels' />
-<div className="text"></div> */}
+//     <div className="img-pic">
+// {/* <img src={homePhoto} alt='TruHome Apparels' />
+// <div className="text"></div> */}
 
 
-    </div>
+//     </div>
+<div>
+  
+</div>
   )
 }
